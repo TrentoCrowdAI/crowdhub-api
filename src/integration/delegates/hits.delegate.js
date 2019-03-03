cc40ed484890a73a945e80abdf3e288fab971a0c
Jorge Ramírez <jorgeramirez1990@gmail.com>
2019-03-03 13:22:48 +0100

    Minor update on doc
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

--- src/integration/delegates/hits.delegate.js
+++ src/integration/delegates/hits.delegate.js
@@ -1,5 +1,6 @@
 // this modules is responsible for allowing to publish HITs in
-// any of the supported platforms
+// any of the supported platforms. Also to implement any other 
+// functionality related to HITs.
 
 const publish = async () => {
   // TODO
