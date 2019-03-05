2549bc788d0aa826592c23d3149e5ba4653fc8cb
Jorge Ramírez <jorgeramirez1990@gmail.com>
2019-03-05 15:20:59 +0100

    Add error handler middleware
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,11 +2,25 @@ global.__base = __dirname;
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const Boom = require('boom');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((err, req, res, next) => {
+  console.error('Error', error);
+
+  if (error.status === 401 && !error.isBoom) {
+    const message = 'Not authorized to perform the request';
+    error = Boom.unauthorized(message);
+  }
+  if (!error.isBoom) {
+    error = Boom.badImplementation();
+  }
+  res.status(error.output.statusCode).send(error.output);
+});
 
 // health check (public endpoint)
 app.get('/', (req, res) => {
