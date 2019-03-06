8bf4d440714b52a5685268062ab108a95b38b9c7
Davide <davide.zanella10@gmail.com>
2019-03-06 11:55:01 +0100

    Add getJob and updateJob DAO

--- src/integration/dao/jobs.dao.js
+++ src/integration/dao/jobs.dao.js
@@ -12,9 +12,31 @@ const createJob = async (job) => {
 };
 
 // get
+const getJob = async (jobId) => {
+    let res = await db.query(
+        `select * from ${db.TABLES.Job} where id = $1`,
+        [jobId]
+    );
+
+    return res.rows[0];
+};
+
 // delete
+
 // update
+const updateJob = async (job) => {
+    let res = await db.query(
+        `update ${db.TABLES.Job} 
+            set updated_at = $1, data = $2
+            where id = $3 returning *`,
+        [new Date(), job.data, jod.id]
+    );
+
+    return res.rows[0];
+};
 
 module.exports = {
-    createJob
+    createJob,
+    getJob,
+    updateJob
 };
