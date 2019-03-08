543fd28a74afb5885fdf9ff5da66a4399eaea4e2
Davide <davide.zanella10@gmail.com>
2019-03-08 11:28:10 +0100

    Add reward info setted and code cleaned

--- src/integration/platforms/f8.js
+++ src/integration/platforms/f8.js
@@ -3,6 +3,10 @@ const querystring = require('querystring');
 
 const config = require(__base + 'config/index');
 
+/**
+ * Publish the job parameter on the F8 platform as a new job.
+ * @param {{}} job 
+ */
 const publish = async (job) => {
     job = await createNewJob(job);
 
@@ -17,17 +21,22 @@ const publish = async (job) => {
     //set the CML of the job
     job = await updateJobCML(job);
 
-    console.log(job);
+    //set the reward info of the job
+    job = await updateJobSpec(job);
 
     return job;
 };
 
+/**
+ * Create a new job on F8
+ * @param {{}} job
+ */
 const createNewJob = async (job) => {
     let url = config.f8.baseEndpoint + 'jobs.json?key=' + config.f8.apiKey;
 
-    //TODO: add more info
     let newJob = {
-        'job[title]': job.data.name
+        'job[title]': job.data.name,
+        'job[instructions]': job.data.instructions
     };
     let body = querystring.stringify(newJob);
 
@@ -42,10 +51,15 @@ const createNewJob = async (job) => {
         job.data.platform = {};
 
     job.data.platform.f8 = json;
-
+    
     return job;
 };
 
+/**
+ * Add some items to an existing F8 job using a CSV file
+ * @param {{}} job 
+ * @param {string} csvFile URL to the CSV file
+ */
 const addCsvItems = async (job, csvFile) => {
     let url = config.f8.baseEndpoint + `jobs/${job.data.platform.f8.id}/upload.json?key=${config.f8.apiKey}&force=true`;
 
@@ -66,10 +80,10 @@ const addCsvItems = async (job, csvFile) => {
     return job;
 };
 
-const updateJobInfo = async (job) => {
-    //TODO
-};
-
+/**
+ * Update the CML of an existing F8 job
+ * @param {{}} job 
+ */
 const updateJobCML = async (job) => {
     let url = config.f8.baseEndpoint + `jobs/${job.data.platform.f8.id}.json?key=${config.f8.apiKey}`;
 
@@ -90,6 +104,36 @@ const updateJobCML = async (job) => {
     return job;
 };
 
+/**
+ * Update the job reward, maxVotes and numVotes on an existing F8 job
+ * @param {{}} job 
+ */
+const updateJobSpec = async (job) => {
+    let url = config.f8.baseEndpoint + `jobs/${job.data.platform.f8.id}.json?key=${config.f8.apiKey}`;
+
+    let data = {
+        'job[payment_cents]': job.data.reward,
+        'job[max_judgments_per_worker]': job.data.maxVotes,
+        'job[judgments_per_unit]': job.data.numVotes
+    };
+    let body = querystring.stringify(data);
+
+    let res = await fetch(url, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: body
+    });
+
+    let json = await res.json();
+    job.data.platform.f8 = json;
+
+    return job;
+};
+
+/**
+ * Convert the loaded Gold items of an existing job into test questions
+ * @param {{}} job 
+ */
 const convertGoldQuestions = async (job) => {
     let url = config.f8.baseEndpoint + `jobs/${job.data.platform.f8.id}/gold.json?key=${config.f8.apiKey}`;
     await fetch(url, {
