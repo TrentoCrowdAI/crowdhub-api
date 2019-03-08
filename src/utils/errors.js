c726dcefb16c627c04f548abb7e1e4baa4952647
Davide <davide.zanella10@gmail.com>
2019-03-08 13:55:23 +0100

    Add business Not Found error

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -14,6 +14,19 @@ const createBusinessError = msg => {
 };
 
 /**
+ * Returns a business Not Found error. The name property
+ * of the error object is set to "businessNotFound".
+ *
+ * @param {String} msg
+ * @returns {Error}
+ */
+const createBusinessNotFoundError = msg => {
+  let e = new Error(msg);
+  e.name = 'businessNotFound';
+  return e;
+};
+
+/**
  * Creates an error that the service layer returns. It uses the boom library.
  *
  * @param {Error} e An error object. The property name should be set
@@ -22,6 +35,9 @@ const createBusinessError = msg => {
 const createServiceError = e => {
   if (e.name == 'business') {
     return Boom.badRequest(e.message);
+  }
+  else if (e.name == 'businessNotFound') {
+    return Boom.notFound(e.message);
   }
   return Boom.badImplementation();
 };
