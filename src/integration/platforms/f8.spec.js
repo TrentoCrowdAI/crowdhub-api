a51dcd6ad5e1c879c0f8c425461e59cdde2a8463
Davide <davide.zanella10@gmail.com>
2019-03-08 17:02:35 +0100

    Add F8 tests

--- src/integration/platforms/f8.spec.js
+++ src/integration/platforms/f8.spec.js
@@ -1,5 +1,26 @@
-describe('Empty test', () => {
-    test('Empty', () => {
-        expect(true).toBe(true);
+const f8 = require('./f8');
+const jobsDelegate = require(__base + 'integration/delegates/jobs.delegate');
+
+const rightJob = require(__base + 'example/job-example.json');
+
+//set jest timeout to 10 seconds
+jest.setTimeout(10000);
+
+describe('F8 tests', async () => {
+    let job;
+
+    beforeAll(async () => {
+        job = await jobsDelegate.createJob(rightJob);
+    });
+
+    test('F8 publish test', async () => {
+        let res = await f8.publish(job);
+
+        expect(res.data.platform.f8).toBeDefined();
+        expect(res.data.platform.f8.id).toBeDefined();
+    });
+
+    afterAll(async () => {
+        await jobsDelegate.deleteJob(job.id);
     });
 });
