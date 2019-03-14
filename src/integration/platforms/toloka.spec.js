a36ddf64def96d2097eebbd10aae2cd4f8273d46
Davide <davide.zanella10@gmail.com>
2019-03-14 16:49:00 +0100

    Add toloka tests

--- src/integration/platforms/toloka.spec.js
+++ src/integration/platforms/toloka.spec.js
@@ -1,5 +1,25 @@
-describe('Empty test', () => {
-    test('Empty', () => {
-        expect(true).toBe(true);
+const toloka = require('./toloka');
+const jobsDelegate = require(__base + 'integration/delegates/jobs.delegate');
+
+const rightJob = require(__base + 'example/job-example-toloka.json');
+
+//set jest timeout to 20 seconds
+jest.setTimeout(20000);
+
+describe('Toloka tests', async () => {
+    let job;
+
+    beforeAll(async () => {
+        job = await jobsDelegate.createJob(rightJob);
+    });
+
+    test('Toloka publish test', async () => {
+        let res = await toloka.publish(job);
+
+        expect(res.data.platform.toloka).toBeDefined();
+    });
+
+    afterAll(async () => {
+        await jobsDelegate.deleteJob(job.id);
     });
 });
