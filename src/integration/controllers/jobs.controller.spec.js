1896046b10ca4dca251eb18fc09d09327b6112a2
Davide <davide.zanella10@gmail.com>
2019-03-14 17:40:10 +0100

    Add tests for the controllers

--- src/integration/controllers/jobs.controller.spec.js
+++ src/integration/controllers/jobs.controller.spec.js
@@ -1,9 +1,11 @@
 const request = require('supertest');
 
 const app = require(__base + 'app');
-const jobsDelegate = require(__base + 'integration/delegates/jobs.delegate');
 
 const rightJob = require(__base + 'example/job-example-f8.json');
+
+//set jest timeout to 20 seconds
+jest.setTimeout(20000);
 
 test('POST /jobs should return 400 if mandatory fields are not present', async () => {
   let response = await request(app).post('/jobs');
@@ -31,16 +33,33 @@ test('POST /jobs/0/publish should return 404', async () => {
   expect(response.status).toBe(404);
 });
 
-describe('Right job insert', async () => {
+test('Right job insert, get, update, delete', async () => {
   let jobRes;
 
-  test('POST /jobs should return 201 ad the id has to be defined if the new job is created', async () => {
-    jobRes = await request(app).post('/jobs').send(rightJob);
-    expect(jobRes.status).toBe(201);
-    expect(jobRes.body.id).toBeDefined();
-  });
+  //POST /jobs should return 201 ad the id has to be defined if the new job is created
+  let response = await request(app).post('/jobs').send(rightJob);
+  expect(response.status).toBe(201);
+  expect(response.body.id).toBeDefined();
+  jobRes = response.body;
 
-  afterAll(async () => {
-    await jobsDelegate.deleteJob(jobRes.id);
-  });
+  //GET /jobs/id should return 200
+  response = await request(app).get('/jobs/' + jobRes.id);
+  expect(response.status).toBe(200);
+  expect(response.body).toBeDefined();
+
+  //PUT /jobs/id should return 200
+  response = await request(app).put('/jobs/' + jobRes.id).send(rightJob);
+  expect(response.status).toBe(200);
+  expect(response.body).toBeDefined();
+
+
+  //DELETE /jobs/id should return 200
+  response = await request(app).delete('/jobs/' + jobRes.id);
+  expect(response.status).toBe(200);
+});
+
+test('GET /jobs should return 200', async () => {
+  let response = await request(app).get('/jobs');
+  expect(response.status).toBe(200);
+  expect(response.body).toBeInstanceOf(Array);
 });
