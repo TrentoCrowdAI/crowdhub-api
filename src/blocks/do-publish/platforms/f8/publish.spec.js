9843f9d9a0461edbbddebc94242f070b2b830bf4
Davide <davide.zanella10@gmail.com>
2019-04-18 19:41:31 +0200

    Add more publishing tests

--- src/blocks/do-publish/platforms/f8/publish.spec.js
+++ src/blocks/do-publish/platforms/f8/publish.spec.js
@@ -2,23 +2,54 @@ const doPublish = require('./publish');
 const templateDoDelegate = require(__base + 'delegates/template-do.delegate');
 
 let example = __base + "../example/";
-const exampleData = require(example + 'job-example-text-highlighting.json');
+const exampleTextHighlighting = require(example + 'job-example-text-highlighting.json');
+const exampleImageClassification = require(example + 'job-example-image-classification.json');
+const exampleImageHighlighting = require(example + 'job-example-image-highlighting.json');
 
 //set jest timeout to 20 seconds
 jest.setTimeout(20000);
 
 describe('F8 tests', async () => {
-    let templateDo;
+    let templateDoIds = [];
 
     beforeAll(async () => {
     });
 
-    test('F8 publish test', async () => {
+    test('F8 publish test [text-highlighting]', async () => {
         //create template-do
-        templateDo = await templateDoDelegate.create(exampleData.template_do);
+        let templateDo = await templateDoDelegate.create(exampleTextHighlighting.template_do);
+        templateDoIds.push(templateDo.id);
 
-        exampleData.blockData.id_template_do = templateDo.id;
-        let res = await doPublish(exampleData.blockData, exampleData.input);
+        exampleTextHighlighting.blockData.id_template_do = templateDo.id;
+        let res = await doPublish(exampleTextHighlighting.blockData, exampleTextHighlighting.input);
+        expect(res).toBeDefined();
+        expect(res.id).toBeDefined();
+        expect(typeof res.cml).toBe("string");
+        expect(typeof res.js).toBe("string");
+        expect(typeof res.css).toBe("string");
+    });
+
+    test('F8 publish test [image-classification]', async () => {
+        //create template-do
+        let templateDo = await templateDoDelegate.create(exampleImageClassification.template_do);
+        templateDoIds.push(templateDo.id);
+
+        exampleImageClassification.blockData.id_template_do = templateDo.id;
+        let res = await doPublish(exampleImageClassification.blockData, exampleImageClassification.input);
+        expect(res).toBeDefined();
+        expect(res.id).toBeDefined();
+        expect(typeof res.cml).toBe("string");
+        expect(typeof res.js).toBe("string");
+        expect(typeof res.css).toBe("string");
+    });
+
+    test('F8 publish test [image-highlighting]', async () => {
+        //create template-do
+        let templateDo = await templateDoDelegate.create(exampleImageHighlighting.template_do);
+        templateDoIds.push(templateDo.id);
+
+        exampleImageHighlighting.blockData.id_template_do = templateDo.id;
+        let res = await doPublish(exampleImageHighlighting.blockData, exampleImageHighlighting.input);
         expect(res).toBeDefined();
         expect(res.id).toBeDefined();
         expect(typeof res.cml).toBe("string");
@@ -27,6 +58,7 @@ describe('F8 tests', async () => {
     });
 
     afterAll(async () => {
-        await templateDoDelegate.deleteTemplateDo(templateDo.id);
+        for (let id of templateDoIds)
+          await templateDoDelegate.deleteTemplateDo(id);
     });
 });
--- src/blocks/do-publish/platforms/toloka/publish.spec.js
+++ src/blocks/do-publish/platforms/toloka/publish.spec.js
@@ -2,35 +2,72 @@ const doPublish = require('./publish');
 const templateDoDelegate = require(__base + 'delegates/template-do.delegate');
 
 let example = __base + "../example/";
-const exampleData = require(example + 'job-example-text-highlighting.json');
+const exampleTextHighlighting = require(example + 'job-example-text-highlighting.json');
+const exampleImageClassification = require(example + 'job-example-image-classification.json');
+const exampleImageHighlighting = require(example + 'job-example-image-highlighting.json');
 
 //set jest timeout to 20 seconds
 jest.setTimeout(20000);
 
 describe('Toloka tests', async () => {
-  let templateDo;
+  let templateDoIds = [];
 
   beforeAll(async () => {
   });
 
-  test('Toloka publish test', async () => {
-      //create template-do
-      templateDo = await templateDoDelegate.create(exampleData.template_do);
+  test('Toloka publish test [text-highlighting]', async () => {
+    //create template-do
+    let templateDo = await templateDoDelegate.create(exampleTextHighlighting.template_do);
+    templateDoIds.push(templateDo.id);
 
-      exampleData.blockData.id_template_do = templateDo.id;
-      let res = await doPublish(exampleData.blockData, exampleData.input);
-      expect(res).toBeDefined();
-      expect(res.id).toBeDefined();
-      expect(typeof res.task_spec.view_spec.markup).toBe("string");
-      expect(typeof res.task_spec.view_spec.script).toBe("string");
-      expect(typeof res.task_spec.view_spec.styles).toBe("string");
+    exampleTextHighlighting.blockData.id_template_do = templateDo.id;
+    let res = await doPublish(exampleTextHighlighting.blockData, exampleTextHighlighting.input);
+    expect(res).toBeDefined();
+    expect(res.id).toBeDefined();
+    expect(typeof res.task_spec.view_spec.markup).toBe("string");
+    expect(typeof res.task_spec.view_spec.script).toBe("string");
+    expect(typeof res.task_spec.view_spec.styles).toBe("string");
 
-      expect(res.taskPool.id).toBeDefined();
-      expect(res.tasks).toBeDefined();
-      
+    expect(res.taskPool.id).toBeDefined();
+    expect(res.tasks).toBeDefined();
+  });
+
+  test('Toloka publish test [image-classification]', async () => {
+    //create template-do
+    let templateDo = await templateDoDelegate.create(exampleImageClassification.template_do);
+    templateDoIds.push(templateDo.id);
+
+    exampleImageClassification.blockData.id_template_do = templateDo.id;
+    let res = await doPublish(exampleImageClassification.blockData, exampleImageClassification.input);
+    expect(res).toBeDefined();
+    expect(res.id).toBeDefined();
+    expect(typeof res.task_spec.view_spec.markup).toBe("string");
+    expect(typeof res.task_spec.view_spec.script).toBe("string");
+    expect(typeof res.task_spec.view_spec.styles).toBe("string");
+
+    expect(res.taskPool.id).toBeDefined();
+    expect(res.tasks).toBeDefined();
+  });
+
+  test('Toloka publish test [image-highlighting]', async () => {
+    //create template-do
+    let templateDo = await templateDoDelegate.create(exampleImageHighlighting.template_do);
+    templateDoIds.push(templateDo.id);
+
+    exampleImageHighlighting.blockData.id_template_do = templateDo.id;
+    let res = await doPublish(exampleImageHighlighting.blockData, exampleImageHighlighting.input);
+    expect(res).toBeDefined();
+    expect(res.id).toBeDefined();
+    expect(typeof res.task_spec.view_spec.markup).toBe("string");
+    expect(typeof res.task_spec.view_spec.script).toBe("string");
+    expect(typeof res.task_spec.view_spec.styles).toBe("string");
+
+    expect(res.taskPool.id).toBeDefined();
+    expect(res.tasks).toBeDefined();
   });
 
   afterAll(async () => {
-      await templateDoDelegate.deleteTemplateDo(templateDo.id);
+    for (let id of templateDoIds)
+      await templateDoDelegate.deleteTemplateDo(id);
   });
 });
