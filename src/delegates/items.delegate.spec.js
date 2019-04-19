cbedcf7e7b6003ed8e606a47ffad40ee582a9715
Davide <davide.zanella10@gmail.com>
2019-04-19 14:40:48 +0200

    Add items delegate tests

--- src/delegates/items.delegate.spec.js
+++ src/delegates/items.delegate.spec.js
@@ -4,28 +4,39 @@ const itemsDao = require(__base + 'dao/items.dao');
 const itemsDelegate = require('./items.delegate');
 
 describe('Items delegate', () => {
-    itemsDao.create.mockReturnValue(Promise.resolve({}));
+    itemsDao.create.mockImplementation((item) => Promise.resolve({ id: 1, data: item }));
     itemsDao.get.mockReturnValue(Promise.resolve({}));
     itemsDao.getAll.mockReturnValue(Promise.resolve({}));
     itemsDao.update.mockReturnValue(Promise.resolve({}));
     itemsDao.deleteItem.mockReturnValue(Promise.resolve({}));
 
-    test('Items tester', () => {
-        let items = {};
+    test('Items should call all CRUD functions', async () => {
+        let item = {};
 
-        itemsDelegate.create(items);
+        let newitem = await itemsDelegate.create(item);
         expect(itemsDao.create).toBeCalled();
 
-        itemsDelegate.get(1);
+        await itemsDelegate.get(newitem.id);
         expect(itemsDao.get).toBeCalled();
 
-        itemsDelegate.getAll();
+        await itemsDelegate.getAll();
         expect(itemsDao.getAll).toBeCalled();
 
-        itemsDelegate.update(items, 1);
+        await itemsDelegate.update(newitem, newitem.id);
         expect(itemsDao.update).toBeCalled();
         
-        itemsDelegate.deleteItem(1);
+        await itemsDelegate.deleteItem(newitem.id);
         expect(itemsDao.deleteItem).toBeCalled();
+    });
+
+    test('Items create should rise error because no name given', async () => {
+        let item = [];
+
+        await expect(itemsDelegate.create(item)).rejects.toThrow();
+    });
+
+    test('Items get should rise error because of wrong id params', async () => {
+        await expect(itemsDelegate.get({})).rejects.toThrow();
+        await expect(itemsDelegate.get('a')).rejects.toThrow();
     });
 });
